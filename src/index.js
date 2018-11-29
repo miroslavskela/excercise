e23835bde9d4697bcd1f1ce4dbf26e9ba3bee905
miroslavskela <mikiskela1992@gmail.com>
2018-11-29 13:44:53 +0100

    minor change

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import {HashRouter} from 'react-router-dom';
 import promise from 'redux-promise'
 import App from './components/app'
 import reducers from './reducers';
